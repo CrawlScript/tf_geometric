a332c786dd1d858b55347cf7d1d5d960e56ddb11
hujunxianligong <hujunxianligong@gmail.com>
2020-02-05 01:05:36 +0800

    update doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -51,6 +51,7 @@ exclude_patterns = []
 html_theme = "sphinx_rtd_theme"
 
 html_show_sourcelink = False
+master_doc = 'index'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
