5335df56c436434bf8d60a34eb270c9ab1588bb9
wangyouze <wangyouze6889@163.com>
2020-07-09 16:54:46 +0800

    second commit

--- tf_geometric/layers/conv/graphSAGE.py
+++ tf_geometric/layers/conv/graphSAGE.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import tensorflow as tf
 
-from tf_geometric.nn.conv.graphSAGE import graphSAGE as  SAGE
+from tf_geometric.nn.conv.graphsage import graphSAGE as  SAGE
 from tf_geometric.layers.kernel.map_reduce import MapReduceGNN
 from tensorflow.python import keras
 
@@ -29,59 +29,90 @@ class GraphSAGE(MapReduceGNN):
         x_shape = input_shapes[0]
         num_features = x_shape[-1]
 
-        ## mean aggregate and gcn aggregate
-        self.kernel = self.add_weight("kernel", shape=[num_features*2, self.units], initializer="glorot_uniform")
-        if self.use_bias:
-            self.bias = self.add_weight("bias", shape=[self.units], initializer="zeros")
+        ## mean aggregate
+        self.mean_kernel = self.add_weight("kernel", shape=[num_features, self.units], initializer="glorot_uniform")
+        self.mean_kernel_2 = self.add_weight("kernel", shape=[num_features, self.units], initializer="glorot_uniform")
 
-        self.hidden_kernel = self.add_weight("kernel", shape=[self.units * 2, self.units],
+        if self.use_bias:
+            self.mean_kernel_bias = self.add_weight("bias", shape=[self.units * 2], initializer="zeros")
+
+        self.mean_hidden_kernel = self.add_weight("kernel", shape=[self.units * 2, self.units],
                                             initializer="glorot_uniform")
+        self.mean_hidden_kernel_2 = self.add_weight("kernel", shape=[self.units * 2 , self.units],
+                                             initializer="glorot_uniform")
         if self.use_bias:
-            self.hidden_bias = self.add_weight("bias", shape=[self.units], initializer="zeros")
+            self.mean_hidden_bias = self.add_weight("bias", shape=[self.units * 2], initializer="zeros")
 
-        self.out_kernel = self.add_weight("kernel", shape=[self.units * 2, self.num_classes],
+        self.mean_out_kernel = self.add_weight("kernel", shape=[self.units * 2, self.units],
                                           initializer="glorot_uniform")
+        self.mean_out_kernel_2 = self.add_weight("kernel", shape=[self.units * 2, self.units],
+                                          initializer="glorot_uniform")
+
         if self.use_bias:
-            self.out_bias = self.add_weight("bias", shape=[self.num_classes], initializer="zeros")
+            self.mean_out_bias = self.add_weight("bias", shape=[self.units * 2], initializer="zeros")
 
+        ##gcn aggregate
+        self.gcn_kernel = self.add_weight("kernel", shape=[num_features * 2, self.units], initializer="glorot_uniform")
+        if self.use_bias:
+            self.gcn_kernel_bias = self.add_weight("bias", shape=[self.units], initializer="zeros")
 
+        self.gcn_hidden_kernel = self.add_weight("kernel", shape=[self.units * 2, self.units], initializer="glorot_uniform")
+        if self.use_bias:
+            self.gcn_hidden_bias = self.add_weight("bias", shape=[self.units], initializer="zeros")
+
+        self.gcn_out_kernel = self.add_weight("kernel", shape=[self.units * 2, self.units * 2], initializer="glorot_uniform")
+        if self.use_bias:
+            self.gcn_out_bias = self.add_weight("bias", shape=[self.units * 2], initializer="zeros")
 
         ##mean_pooling and max_pooling
-        self.pooling_kernel = self.add_weight("kernel", shape=[num_features, self.units], initializer="glorot_uniform")
+        self.first__mlp_kernel = self.add_weight("kernel", shape=[num_features, self.units], initializer="glorot_uniform")
         if self.use_bias:
-            self.pooling_bias = self.add_weight("bias", shape=[self.units], initializer="zeros")
+            self.first_mlp_bias = self.add_weight("bias", shape=[self.units], initializer="zeros")
 
-        self.kernel_2 = self.add_weight("kernel", shape=[self.units + num_features, self.units],
+        self.first_pooling_kernel = self.add_weight("kernel", shape=[self.units, self.units],
                                              initializer="glorot_uniform")
+        self.first_pooling_kernel_2 = self.add_weight("kernel", shape=[num_features, self.units],
+                                              initializer="glorot_uniform")
         if self.use_bias:
-            self.bias_2 = self.add_weight("bias", shape=[self.units], initializer="zeros")
+            self.first_pooling_bias = self.add_weight("bias", shape=[self.units * 2], initializer="zeros")
 
+
+        self.hidden_mlp_kernel = self.add_weight("kernel", shape=[self.units * 2, self.units],
+                                                     initializer="glorot_uniform")
         self.hidden_pooling_kernel = self.add_weight("kernel", shape=[self.units, self.units],
+                                                     initializer="glorot_uniform")
+        self.hidden_pooling_kernel_2 = self.add_weight("kernel", shape=[self.units * 2, self.units],
                                              initializer="glorot_uniform")
         if self.use_bias:
-            self.hidden_pooling_bias = self.add_weight("bias", shape=[self.units], initializer="zeros")
-
-        self.kernel_3 = self.add_weight("kernel", shape=[self.units * 2, self.units],
-                                          initializer="glorot_uniform")
+            self.hidden_mlp_bias = self.add_weight("bias", shape=[self.units], initializer="zeros")
         if self.use_bias:
-            self.bias_3 = self.add_weight("bias", shape=[self.units], initializer="zeros")
+            self.hidden_pooling_bias = self.add_weight("bias", shape=[self.units * 2], initializer="zeros")
 
+
+
+        self.out_mlp_kernel = self.add_weight("kernel", shape=[self.units * 2, self.units],
+                                        initializer="glorot_uniform")
         self.out_pooling_kernel = self.add_weight("kernel", shape=[self.units, self.units],
+                                                  initializer="glorot_uniform")
+        self.out_pooling_kernel_2 = self.add_weight("kernel", shape=[self.units * 2, self.units],
+                                                  initializer="glorot_uniform")
+
+        if self.use_bias:
+            self.out_mlp_bias = self.add_weight("bias", shape=[self.units], initializer="zeros")
+        if self.use_bias:
+            self.out_pooling_bias = self.add_weight("bias", shape=[self.units * 2], initializer="zeros")
+
+
+        self.classifier = self.add_weight("kernel", shape=[self.units * 2, self.num_classes],
                                         initializer="glorot_uniform")
         if self.use_bias:
-            self.out_pooling_bias = self.add_weight("bias", shape=[self.units], initializer="zeros")
-
-        self.kernel_4 = self.add_weight("kernel", shape=[self.units * 2, self.num_classes],
-                                        initializer="glorot_uniform")
-        if self.use_bias:
-            self.bias_4 = self.add_weight("bias", shape=[self.num_classes], initializer="zeros")
-
+            self.classifier_bias = self.add_weight("bias", shape=[self.num_classes], initializer="zeros")
 
         self.dropout = keras.layers.Dropout(self.drop_rate)
 
 
 
-    def call(self, inputs, num_layers=3, training=None):
+    def call(self, inputs, num_layers=3, training=False):
 
         if len(inputs) == 3:
             x, edge_index, edge_weight = inputs
@@ -90,49 +121,82 @@ class GraphSAGE(MapReduceGNN):
             edge_weight = tf.ones(edge_index.shape[-1],dtype=tf.int32)
 
         if self.aggregate_type == 'mean_pooling' or self.aggregate_type == 'max_pooling':
-            kernel = self.pooling_kernel
-            bias = self.pooling_bias
-            kernel_2 = self.kernel_2
-            bias_2 = self.bias_2
-            hidden_kernel = self.hidden_pooling_kernel
-            hidden_bias = self.hidden_pooling_bias
-            hidden_kernel_2 = self.kernel_3
-            hidden_bias_2 = self.bias_3
-            out_kernel = self.out_pooling_kernel
-            out_bias = self.out_pooling_bias
-            out_kernel_2 = self.kernel_4
-            out_bias_2 = self.bias_4
+            kernel = self.first__mlp_kernel
+            bias = self.first_mlp_bias
+            kernel_2 = self.first_pooling_kernel
+            kernel_3 = self.first_pooling_kernel_2
+            bias_2 = self.first_pooling_bias
+
+            hidden_kernel = self.hidden_mlp_kernel
+            hidden_bias = self.hidden_mlp_bias
+            hidden_kernel_2 = self.hidden_pooling_kernel
+            hidden_kernel_3 = self.hidden_pooling_kernel_2
+            hidden_bias_2 = self.hidden_pooling_bias
+
+            out_kernel = self.out_mlp_kernel
+            out_bias = self.out_mlp_bias
+            out_kernel_2 = self.out_pooling_kernel
+            out_kernel_3 = self.out_pooling_kernel_2
+            out_bias_2 = self.out_pooling_bias
 
 
-        else:
-            kernel = self.kernel
-            bias = self.bias
-            kernel_2 = None
+        elif self.aggregate_type == 'mean':
+            kernel = self.mean_kernel
+            kernel_2 = self.mean_kernel_2
+            kernel_3 = None
+            bias = self.mean_kernel_bias
             bias_2 = None
-            hidden_kernel = self.hidden_kernel
-            hidden_bias = self.hidden_bias
+
+            hidden_kernel = self.mean_hidden_kernel
+            hidden_bias = self.mean_hidden_bias
+            hidden_kernel_2 = self.mean_hidden_kernel_2
+            hidden_bias_2 = None
+            hidden_kernel_3 = None
+
+            out_kernel = self.mean_out_kernel
+            out_bias = self.mean_out_bias
+            out_kernel_2 = self.mean_out_kernel_2
+            out_bias_2 = None
+            out_kernel_3 = None
+
+        elif self.aggregate_type == 'gcn':
+            kernel = self.gcn_kernel
+            kernel_2 = None
+            kernel_3 = None
+            bias = self.gcn_kernel_bias
+            bias_2 = None
+
+            hidden_kernel = self.gcn_hidden_kernel
+            hidden_bias = self.gcn_hidden_bias
             hidden_kernel_2 = None
             hidden_bias_2 = None
-            out_kernel = self.out_kernel
-            out_bias = self.out_bias
+            hidden_kernel_3 = None
+
+            out_kernel = self.gcn_out_kernel
+            out_bias = self.gcn_out_bias
             out_kernel_2 = None
             out_bias_2 = None
+            out_kernel_3 = None
 
 
 
-        h = SAGE(x, edge_index[0], edge_weight[0], kernel, kernel_2, bias, bias_2,
+        h = SAGE(x, edge_index[0], edge_weight[0], kernel, kernel_2, kernel_3, bias, bias_2,
                  aggregate_type=self.aggregate_type, activation=self.acvitation)
 
+
         for i in range(1,num_layers-1):
-            h = SAGE(h, edge_index[i], edge_weight[i], hidden_kernel, hidden_kernel_2, hidden_bias,
+            h = SAGE(h, edge_index[i], edge_weight[i], hidden_kernel, hidden_kernel_2, hidden_kernel_3,hidden_bias,
                      hidden_bias_2,  aggregate_type=self.aggregate_type, activation=self.acvitation)
 
             if i != num_layers-1:
                 h = self.dropout(h,training=training)
 
-        out = SAGE(h, edge_index[-1], edge_weight[-1], out_kernel, out_kernel_2, out_bias, out_bias_2,
-                   aggregate_type=self.aggregate_type, activation=self.acvitation)
+        h = SAGE(h, edge_index[-1], edge_weight[-1], out_kernel, out_kernel_2, out_kernel_3, out_bias, out_bias_2,
+                 aggregate_type=self.aggregate_type, activation=self.acvitation)
 
 
+        out =  h @ self.classifier
+        out += self.classifier_bias
+        # out = tf.nn.relu(out)
         return out
 
--- tf_geometric/nn/conv/graphSAGE.py
+++ tf_geometric/nn/conv/graphSAGE.py
@@ -8,8 +8,6 @@ from tf_geometric.nn.kernel.map_reduce import sum_reducer
 
 
 
-# def sum_reducer(neighbor_msg, node_index, num_nodes=None):
-#     return tf.math.unsorted_segment_sum(neighbor_msg, node_index, num_segments=num_nodes)
 
 def mean_reducer(neighbor_msg, node_index, num_nodes=None):
     return tf.math.unsorted_segment_mean(neighbor_msg, node_index, num_segments=num_nodes)
@@ -22,21 +20,23 @@ def max_reducer(neighbor_msg, node_index, num_nodes=None):
     return max_x
 
 
-def mean_aggregate_neighbors(x, edge_index, kernel, edge_weight=None, bias=None,
-                            activation=None, normalize=False):
+
+def mean_aggregate_neighbors(x, edge_index, kernel_1, kernel_2, edge_weight=None, bias=None,
+                             activation=None, normalize=False):
     row, col = edge_index
     repeated_x = tf.gather(x, row)
     neighbor_x = tf.gather(x, col)
 
-
     if edge_weight is not None:
         neighbor_x = gcn_mapper(repeated_x, neighbor_x, edge_weight=edge_weight)
 
-    reduced_msg = mean_reducer(neighbor_x, row, num_nodes=len(x))
 
-    updated_msg = tf.concat([reduced_msg, x], axis=1)
+    neighbor_reduced_msg = mean_reducer(neighbor_x, row, num_nodes=len(x))
 
-    h = updated_msg @ kernel
+    neighbor_msg = neighbor_reduced_msg @ kernel_1
+    x = x @ kernel_2
+    h = tf.concat([neighbor_msg, x], axis=1)
+
     if bias is not None:
         h += bias
 
@@ -61,9 +61,10 @@ def gcn_aggregate_neighbors(x, edge_index, kernel, edge_weight=None, bias=None,
     neighbor_x = tf.gather(x, col)
 
     neighbor_x = gcn_mapper(repeated_x, neighbor_x, edge_weight=edge_weight)
-    reduced_msg = mean_reducer(neighbor_x, row, num_nodes=len(x))
 
+    reduced_msg = mean_reducer(neighbor_x, row, num_nodes=len(x))
     updated_msg = tf.concat([reduced_msg, x], axis=1)
+
 
     h = updated_msg @ kernel
     if bias is not None:
@@ -77,7 +78,7 @@ def gcn_aggregate_neighbors(x, edge_index, kernel, edge_weight=None, bias=None,
 
     return h
 
-def mean_pooling_aggregate_neighbors(x, edge_index, kernel_1, kernel_2, edge_weight=None, bias_1=None,
+def mean_pooling_aggregate_neighbors(x, edge_index, kernel_1, kernel_2, kernel_3, edge_weight=None, bias_1=None,
                             bias_2=None, activation=None, normalize=False):
 
     if edge_weight is not None:
@@ -88,7 +89,6 @@ def mean_pooling_aggregate_neighbors(x, edge_index, kernel_1, kernel_2, edge_wei
     row, col = edge_index
     repeated_x = tf.gather(x, row)
     neighbor_x = tf.gather(x, col)
-
 
     neighbor_x = gcn_mapper(repeated_x, neighbor_x, edge_weight=edge_weight)
 
@@ -101,20 +101,23 @@ def mean_pooling_aggregate_neighbors(x, edge_index, kernel_1, kernel_2, edge_wei
 
     reduced_h = mean_reducer(h, row, num_nodes=len(x))
 
-    h = tf.concat([reduced_h, x],axis=1) @ kernel_2
+    from_neighs = reduced_h @ kernel_2
+    from_x = x @ kernel_3
+
+    output = tf.concat([from_neighs, from_x],axis=1)
     if bias_2 is not None:
-        h += bias_2
+        output += bias_2
 
     if activation is not None:
-        h = activation(h)
+        output = activation(output)
 
     if normalize:
-        h = tf.nn.l2_normalize(h,axis=-1)
+        output = tf.nn.l2_normalize(output,axis=-1)
 
-    return h
+    return output
 
 
-def max_pooling_aggregate_neighbors(x, edge_index, kernel_1, kernel_2, edge_weight=None, bias_1=None,
+def max_pooling_aggregate_neighbors(x, edge_index, kernel_1, kernel_2, kernel_3, edge_weight=None, bias_1=None,
                             bias_2=None, activation=None, normalize=False):
 
     if edge_weight is not None:
@@ -123,7 +126,6 @@ def max_pooling_aggregate_neighbors(x, edge_index, kernel_1, kernel_2, edge_weig
     # edge_index, edge_weight = add_self_loop_edge(edge_index, x.shape[0], edge_weight=edge_weight, fill_weight=2.0)
 
     row, col = edge_index
-
     repeated_x = tf.gather(x, row)
     neighbor_x = tf.gather(x, col)
 
@@ -132,93 +134,57 @@ def max_pooling_aggregate_neighbors(x, edge_index, kernel_1, kernel_2, edge_weig
     h = neighbor_x @ kernel_1
     if bias_1 is not None:
         h += bias_1
+
     if activation is not None:
         h = activation(h)
 
     reduced_h = max_reducer(h, row, num_nodes=len(x))
 
-    h = tf.concat([reduced_h, x], axis=1) @ kernel_2
+    from_neighs = reduced_h @ kernel_2
+    from_x = x @ kernel_3
+
+    output = tf.concat([from_neighs, from_x],axis=1)
     if bias_2 is not None:
-        h += bias_2
+        output += bias_2
+
     if activation is not None:
-        h = activation(h)
+        output = activation(output)
 
     if normalize:
-        h = tf.nn.l2_normalize(h, axis=-1)
+        output = tf.nn.l2_normalize(output,axis=-1)
 
-    return h
-
-# def lstm_aggregate_neighbors(x, edge_index, kernel_1, kernel_2=None, edge_weight=None, bias_1=None, bias_2=None,
-#                 activation=None, normalize=False):
-#
-#     lstm = kernel_1
-#
-#     row, col = edge_index
-#
-#     nodes = tf.unique(row)
-#
-#     row = row.numpy()
-#     nodes_ = nodes[0].numpy()
-#
-#     neighbor_feature = []
-#     for i, v in enumerate(nodes_):
-#         index = np.where(row == i)
-#
-#         neighbor_x = tf.gather(x, edge_index[1][index[0][0]:index[0][-1]+1])
-#         # paddings = [[0, size - len(index[0])], [0, 0]]
-#         # neighbor_x = tf.pad(neighbor_x, paddings,"CONSTANT")
-#
-#         neighbor_x = tf.expand_dims(neighbor_x, axis=0)
-#
-#         h = lstm(neighbor_x)
-#         neighbor_feature.append(h)
-#
-#     h = np.array(neighbor_feature).squeeze(axis=1)
-#
-#     # h = lstm(neighbor_x)
-#
-#     # repeated_x = tf.gather(x, row)
-#     # if edge_weight is not None:
-#     #     h = gcn_mapper(repeated_x, h, edge_weight=edge_weight)
-#
-#     h = tf.concat([h, x], axis=1) @ kernel_2
-#
-#     if bias_1 is not None:
-#         h += bias_2
-#
-#     if activation is not None:
-#         h = activation(h)
-#
-#     if normalize:
-#         h = tf.nn.l2_normalize(h, axis=-1)
-#
-#     return h
+    return output
 
 
-def graphSAGE(x, edge_index, edge_weight, kernel_1, kernel_2=None, bias_1=None, bias_2=None,
+
+def graphSAGE(x, edge_index, edge_weight, kernel_1, kernel_2=None, kernel_3=None, bias_1=None, bias_2=None,
               aggregate_type='mean', activation=None, normalize=False):
     """
 
-       :param x: Tensor, shape: [num_nodes, num_features], node features
-       :param edge_index: Tensor, shape: [2, num_edges], edge information
-       :param edge_weight: Tensor or None, shape: [num_edges]
-       :param kernel_1: Tensor, shape: [num_features*2, num_output_features], weight
-       :param bias_1: Tensor, shape: [num_output_features], bias
-       :param kernel_2: Tensor, shape: [num_features+num_hidden_feature, num_output_features], weight
-       :param bias_2: Tensor, shape: [num_output_features], bias
-       :param activation: Activation function to use.
-       :param normalize: If set to :obj:`True`, output features
-            will be :math:`\ell_2`-normalized, *i.e.*,
-            :math:`\frac{\mathbf{x}^{\prime}_i}
-            {\| \mathbf{x}^{\prime}_i \|_2}`.
-            (default: :obj:`False`)
-       :return: Updated node features (x), shape: [num_nodes, num_output_features]
-       """
+           :param x: Tensor, shape: [num_nodes, num_features], node features
+           :param edge_index: Tensor, shape: [2, num_edges], edge information
+           :param edge_weight: Tensor or None, shape: [num_edges]
+           :param kernel_1: Tensor, in "mean_pooling" or "max_pooling": shape: [num_features, hidden_dim], weight;
+           in "mean", shape: [num_features, hidden_dim], weight; in "gcn", shape[num_features*2, hidden_dim], weight;
+           :param kernel_2: Tensor, in "mean_pooling" or "max_pooling": shape: [hidden_dim, hidden_dim], weight;
+           in "mean", shape: [num_features, hidden_dim], weight; in "gcn", the kernel is none.
+           :param kernel_3: Tensor, in "mean_pooling" or "max_pooling": shape: [num_features, hidden_dim], weight;
+           in "mean",the kernel is none; in "gcn",the kernel is none.
+           :param bias_1: Tensor, in "mean_pooling" or "max_pooling" or "gcn", shape: [hidden_dim], bias; in "mean", shape: [hidden_dim * 2], bias.
+           :param bias_2: Tensor, in "mean_pooling" or "max_pooling", shape: [hidden_dim * 2], bias; in "gcn" and "mean", none.
+           :param activation: Activation function to use.
+           :param normalize: If set to :obj:`True`, output features
+                will be :math:`\ell_2`-normalized, *i.e.*,
+                :math:`\frac{\mathbf{x}^{\prime}_i}
+                {\| \mathbf{x}^{\prime}_i \|_2}`.
+                (default: :obj:`False`)
+           :return: Updated node features (x), shape: [num_nodes, num_output_features]
+           """
 
     if aggregate_type == 'mean':
 
-        h = mean_aggregate_neighbors(x, edge_index, kernel_1, edge_weight=edge_weight, bias=bias_1,
-                            activation=activation, normalize=normalize)
+        h = mean_aggregate_neighbors(x, edge_index, kernel_1, kernel_2,  edge_weight=edge_weight, bias=bias_1,
+                                     activation=activation, normalize=normalize)
 
     if aggregate_type == 'gcn':
 
@@ -228,12 +194,12 @@ def graphSAGE(x, edge_index, edge_weight, kernel_1, kernel_2=None, bias_1=None, 
 
     if aggregate_type == 'mean_pooling':
 
-        h = mean_pooling_aggregate_neighbors(x, edge_index, kernel_1, kernel_2, edge_weight=edge_weight,
+        h = mean_pooling_aggregate_neighbors(x, edge_index, kernel_1, kernel_2, kernel_3, edge_weight=edge_weight,
                                              bias_1=bias_1, bias_2=bias_2, activation=activation, normalize=normalize)
 
     if aggregate_type == 'max_pooling':
 
-        h = max_pooling_aggregate_neighbors(x, edge_index, kernel_1, kernel_2, edge_weight=edge_weight,
+        h = max_pooling_aggregate_neighbors(x, edge_index, kernel_1, kernel_2, kernel_3, edge_weight=edge_weight,
                                              bias_1=bias_1, bias_2=bias_2, activation=activation, normalize=normalize)
 
 
