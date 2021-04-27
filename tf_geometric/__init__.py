56adec8fda5abfc40c78a476871018cca57b1fda
hujunxianligong <hujunxianligong@gmail.com>
2021-04-28 05:08:44 +0800

    update doc

--- tf_geometric/__init__.py
+++ tf_geometric/__init__.py
@@ -4,12 +4,13 @@ import tensorflow as tf
 if tf.__version__[0] == "1":
     tf.enable_eager_execution()
 
-import tf_geometric.nn as nn
-import tf_geometric.utils as utils
-import tf_geometric.data as data
-import tf_geometric.datasets as datasets
-import tf_geometric.layers as layers
+# import tf_geometric.nn as nn
+# import tf_geometric.utils as utils
+# import tf_geometric.data as data
+# import tf_geometric.datasets as datasets
+# import tf_geometric.layers as layers
 
+from . import nn, utils, data, datasets, layers
 from tf_geometric.data.graph import Graph, BatchGraph
 
 
