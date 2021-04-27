a80718a66fe1bbd9940a150ab6491a83257a6371
hujunxianligong <hujunxianligong@gmail.com>
2021-04-28 04:58:04 +0800

    remove utils autoimport

--- tf_geometric/utils/__init__.py
+++ tf_geometric/utils/__init__.py
@@ -1,6 +1,1 @@
 # coding=utf-8
-
-import tf_geometric.utils.graph_utils as graph_utils
-import tf_geometric.utils.data_utils as data_utils
-import tf_geometric.utils.union_utils as union_utils
-import tf_geometric.utils.tf_utils as tf_utils
