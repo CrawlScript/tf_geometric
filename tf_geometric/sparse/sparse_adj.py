45acf6c09208bb1317f26efb266c699cd6682593
hujunxianligong <hujunxianligong@gmail.com>
2021-07-31 15:16:27 +0800

    rename cache_normed_edge => build_cache_for_graph

--- tf_geometric/sparse/sparse_adj.py
+++ tf_geometric/sparse/sparse_adj.py
@@ -159,16 +159,3 @@ class SparseAdj(object):
 
     def __repr__(self):
         return self.__str__()
-
-# edge_index = [
-#     [0, 0, 0, 0, 1, 1, 4, 6],
-#     [0, 2, 4, 6, 2, 3, 6, 8]
-# ]
-#
-# adj = SparseAdj(edge_index)
-# print("==========")
-# print(adj.reduce_sum(axis=-1, keepdims=True))
-# # h = np.random.randn(9, 20).astype(np.float32)
-# #
-# # print(adj @ h)
-# # print(adj.softmax(axis=-1))
