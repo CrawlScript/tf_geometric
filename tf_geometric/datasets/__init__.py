b1938a0eaab164122813bfc8383c379f0daf12bb
zxr <1034823462@qq.com>
2021-08-17 19:18:04 +0800

    add dataloader for blogcatalog dataset

--- tf_geometric/datasets/__init__.py
+++ tf_geometric/datasets/__init__.py
@@ -3,6 +3,6 @@ from tf_geometric.datasets.ppi import PPIDataset
 from tf_geometric.datasets.tu import TUDataset
 from tf_geometric.datasets.planetoid import PlanetoidDataset, CoraDataset, CiteseerDataset, PubmedDataset, \
     SupervisedCoraDataset, SupervisedCiteseerDataset, SupervisedPubmedDataset
-from tf_geometric.datasets.blogcatalog import MultiLabelDataset, BlogcatalogDataset
+from tf_geometric.datasets.blogcatalog import BlogcatalogDataset
 from tf_geometric.datasets.reddit import TransductiveRedditDataset, InductiveRedditDataset
 from tf_geometric.datasets.ogb import OGBNodePropPredDataset
