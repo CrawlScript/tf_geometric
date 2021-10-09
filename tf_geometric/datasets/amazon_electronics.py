63e0118c19726dfd2a9c853bc54f3d72a8ded812
hujunxianligong <hujunxianligong@gmail.com>
2021-10-09 20:45:29 +0800

    bug fix

--- tf_geometric/datasets/amazon_electronics.py
+++ tf_geometric/datasets/amazon_electronics.py
@@ -35,17 +35,18 @@ class AmazonElectronicsDataset(DownloadableDataset):
     def process(self):
 
         npz_path = os.path.join(self.raw_root_path, "amazon_electronics_{}.npz".format(self.dataset_name.replace("amazon-", "")))
-        data = np.load(npz_path)
 
-        x = sp.csr_matrix((data["attr_data"], data["attr_indices"], data["attr_indptr"]), data["attr_shape"]).todense().astype(np.float32)
-        x[x > 0.0] = 1.0
+        with np.load(npz_path) as data:
 
-        adj = sp.csr_matrix((data["adj_data"], data["adj_indices"], data["adj_indptr"]), data["adj_shape"]).tocoo()
-        edge_index = np.stack([adj.row, adj.col], axis=0).astype(np.int32)
-        edge_index, _ = remove_self_loop_edge(edge_index)
-        edge_index, _ = convert_edge_to_directed(edge_index, merge_modes="max")
+            x = sp.csr_matrix((data["attr_data"], data["attr_indices"], data["attr_indptr"]), data["attr_shape"]).todense().astype(np.float32)
+            x[x > 0.0] = 1.0
 
-        y = data["labels"].astype(np.int32)
+            adj = sp.csr_matrix((data["adj_data"], data["adj_indices"], data["adj_indptr"]), data["adj_shape"]).tocoo()
+            edge_index = np.stack([adj.row, adj.col], axis=0).astype(np.int32)
+            edge_index, _ = remove_self_loop_edge(edge_index)
+            edge_index, _ = convert_edge_to_directed(edge_index, merge_modes="max")
+
+            y = data["labels"].astype(np.int32)
 
         graph = Graph(x=x, edge_index=edge_index, y=y)
 
