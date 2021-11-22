4eed169a390d26f65db7843b91b03a739a84d957
hujunxianligong <hujunxianligong@gmail.com>
2021-11-23 03:55:56 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="tf_geometric",
     python_requires='>3.5.0',
-    version="0.0.74",
+    version="0.0.75",
     author="Jun Hu",
     author_email="hujunxianligong@gmail.com",
     packages=find_packages(
